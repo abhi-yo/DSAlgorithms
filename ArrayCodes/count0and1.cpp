570721d1e83ebd8217806ac15eb09f33677b7675
Akshat <112682702+abhi-yo@users.noreply.github.com>
2023-02-14 23:38:53 +0530

    Update count0and1.cpp

---
+++ ArrayCodes/count0and1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void printZero(){
+    int arr[]={1,0,1,1,1,0,0,1};
+    int size=7;
+    int numZero=0;
+    int numOne=0;
+    for(int i=0;i<size;i++){
+        if(arr[i]==0){
+            numZero++;
+        }if(arr[i]==1){
+            numOne++;
+        }
+    }
+}
+int main(){
+    for(int i = 0;i<5; i++){
+        printZero();
+    }
+    return 0;
+}
