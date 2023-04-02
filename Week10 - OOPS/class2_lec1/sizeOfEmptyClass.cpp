58ec8b3a229540a242c47ed81c07f8d29addaf1c
Akshat <112682702+abhi-yo@users.noreply.github.com>
2023-04-02 15:12:01 +0530

    Delete Week10 - OOPS/class2_lec1 directory

--- Week10 - OOPS/class2_lec1/sizeOfEmptyClass.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-class Animal{
-
-};
-int main(){
-    cout<<"Size of empty class is: "<<sizeof(Animal)<<endl;
-    //size is 1 because that is the minimum memory required to show the class.
-}
